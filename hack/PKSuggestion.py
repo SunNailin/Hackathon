b112e6ecf691cc0a029dccf3dcf80ec3d3f7eedf
Lizhaoqiang <ArjenLee9307@gmail.com>
2019-04-10 11:28:21 +0800

    去掉分号。。。。。。。。。。。。。

--- hack/PKSuggestion.py
+++ hack/PKSuggestion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import re
-from mysqlConnect import MysqlConnect 
+from .mysqlConnect import MysqlConnect 
 
 
 clasee PKSuggestion:
@@ -10,36 +10,36 @@ clasee PKSuggestion:
 		
 	def get_PlayerProperties(conn,table,index,value):
 		if not conn:
-			return;
-		db = conn.db;
-		cursor = conn.db.cursor();
-		sql = "SELECT * FROM %s WHERE %s = %s" % (table,index,value);
+			return
+		db = conn.db
+		cursor = conn.db.cursor()
+		sql = "SELECT * FROM %s WHERE %s = %s" % (table,index,value)
 		try:
-			cursor.execute(sql);
-			data = cursor.fetchone();
-			#print(data);
-			return data;
+			cursor.execute(sql)
+			data = cursor.fetchone()
+			#print(data)
+			return data
 		except:
-			print "Error: unable to fecth data!!!";
+			print "Error: unable to fecth data!!!"
 
 	def start_suggestion(str):
 		if re.search(u'PK',str):
-			data = get_PlayerProperties(conn,'t_player','name',"'吴鸭子'");
-			level = data[4];
-			money = data[8];
-			weapon = data[10];
-			armour = data[12];
-			weaponlevel = data[11];
-			armourlevel = data[13];
-			skilllevel = data[14];
-			itemid = data[15];
-			vip = data[9];
-			playerid = data[0];
+			data = get_PlayerProperties(conn,'t_player','name',"'吴鸭子'")
+			level = data[4]
+			money = data[8]
+			weapon = data[10]
+			armour = data[12]
+			weaponlevel = data[11]
+			armourlevel = data[13]
+			skilllevel = data[14]
+			itemid = data[15]
+			vip = data[9]
+			playerid = data[0]
 		
-			my_suggestion = money_spend_suggestion(playerid,level,money,weapon,armour,weaponlevel,armourlevel,skilllevel,vip);
-			#print(my_suggestion);
-			return my_suggestion;
-		return '';
+			my_suggestion = money_spend_suggestion(playerid,level,money,weapon,armour,weaponlevel,armourlevel,skilllevel,vip)
+			#print(my_suggestion)
+			return my_suggestion
+		return ''
 	
 	def get_sectDescriptions(selfid, oppid):
 	
--- hack/mysqlConnect.py
+++ hack/mysqlConnect.py
@@ -3,32 +3,32 @@
 import pymysql
 
 class MysqlConnect:
-	instance = None;
+	instance = None
 	
 	def __init__(self,name):
-		self.name = name;
-		self.db = pymysql.connect("localhost","root","12345","Hackathon",charset='utf8');
-		self.cursor = self.db.cursor();
+		self.name = name
+		self.db = pymysql.connect("localhost","root","12345","Hackathon",charset='utf8')
+		self.cursor = self.db.cursor()
 	
 	def __del__(self):
 		if self.db != None:
-			self.db.close();
-			self.cursor = None;
+			self.db.close()
+			self.cursor = None
 	
 	@classmethod
 	def get_instance(cls,name):
 		if cls.instance:
 			return cls.instance
 		else:
-			obj = cls(name);
-			cls.instance = obj;
-			return obj;
+			obj = cls(name)
+			cls.instance = obj
+			return obj
 
 if __name__ == '__main__':
-	conn1 = MysqlConnect.get_instance('db');
-	conn2 = MysqlConnect.get_instance('sb2');
-	print(conn1);
-	print(conn2);
+	conn1 = MysqlConnect.get_instance('db')
+	conn2 = MysqlConnect.get_instance('sb2')
+	print(conn1)
+	print(conn2)
 
 
 	
