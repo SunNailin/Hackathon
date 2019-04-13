0ca003a6cc6c19fb28db2d23e8ca788d841512a0
sunnailin <sdsunnailin@163.com>
2019-04-13 09:56:59 +0800

    更新数据库更新文件

--- dbUpdate.sql
+++ dbUpdate.sql
@@ -1,11 +1,11 @@
 /*
  * 删除无用活动
  */
-DELETE FROM `hackathon`.`t_activities` WHERE `id` = 4;
-DELETE FROM `hackathon`.`t_activities` WHERE `id` = 5;
+DELETE FROM `t_activities` WHERE `id` = 4;
+DELETE FROM `t_activities` WHERE `id` = 5;
 /*
  * 更改活动时间
  */
-UPDATE `hackathon`.`t_activities` SET `time` = '11:30' WHERE `id` = 1;
-UPDATE `hackathon`.`t_activities` SET `time` = '11:45' WHERE `id` = 2;
-UPDATE `hackathon`.`t_activities` SET `time` = '12:00' WHERE `id` = 3;
+UPDATE `t_activities` SET `time` = '11:30' WHERE `id` = 1;
+UPDATE `t_activities` SET `time` = '11:45' WHERE `id` = 2;
+UPDATE `t_activities` SET `time` = '12:00' WHERE `id` = 3;
